8b48f721d96c28e0bde22776b4093d8428af03a3
hieulacos208 <hieulucas@gmail.com>
2021-03-12 21:40:49 +0700

    ok

---
+++ session1/index.js
@@ -0,0 +1,69 @@
+// // alert(3*10
+// // let s = 'hello world');
+
+// // s = 'hi there';
+
+// // alert(s);
+// // let yourName = prompt('what is your name ?');
+// // console.log(yourName);
+// console.log('cau 1: trong JS nhung truong hop ten bien khong hop le la');
+// console.log('bat dau bang so');
+// console.log('chua ky tu dac biet(ke ca khoang trang) ngoai tru "_" va "$"');
+// console.log('trung voi tu khoa(keywords) cua JavaScript');
+// console.log('vd1 : 1990(bat dau bang so)');
+// console.log('vd2 : hieu@(chua ky tu dac biet)');
+// console.log('vd3 : break(trung voi tu khoa)');
+// console.log('trong JS de kiem tra du lieu cua 1bien ta su dung toan tu typeof');
+
+// console.log('cau 2: 1b. 2a. 3e. 4c');
+
+// let message = 'Coding is great';
+// console.log(message);
+// let studentCount = 0;
+// console.log(studentCount);
+
+// message = 'Coding might not be easy, but still great';
+// console.log(message);
+// studentCount = 17;
+// console.log(studentCount);
+// message = message.toLowerCase();
+// console.log(message);
+// studentCount++;
+// console.log(studentCount);
+
+// alert('You look beautiful day');
+
+// let yourName = prompt('Hi there, your name please?');
+// alert('Hi '+ yourName +', Nice to meet you !');
+
+// let firstName = prompt('Enter your first name');
+// let lastName = prompt('Enter your last name');
+// alert('Hi '+ lastName +' '+ firstName);
+
+// let lengthSquare = prompt('Enter side length of square');
+// alert(`The square area is  ${lengthSquare*lengthSquare}`);
+
+// let r = prompt('Enter the radius of the circle');
+// alert(`The circle area is ${(r*r*3.14)}`);
+
+// let c = prompt('Enter the temperature in Celcius');
+// alert(`The Fahenheit is ${(c*1.8)+32}`);
+
+
+
+// let x = 0;
+// while (x < 3) {
+//     console.log('hieu');
+//     x++;
+// }
+
+// for (let x = 1; x <= 10; x++) {alert(x);
+// }
+
+
+ 
+let age = prompt('nhap tuoi cua ban');
+if (age < 10) {
+    alert('di an kem');
+} else {alert('di xem phim');
+}
