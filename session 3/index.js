25b061adb4f65bb98b143c8e61f52bd58b166c4c
hieulacos208 <hieulucas@gmail.com>
2021-03-23 08:01:57 +0700

    ok

---
+++ session 3/index.js
@@ -0,0 +1,85 @@
+// cau 1 ;
+      // 1.a ;
+         //  let a = 5 ;
+         //  let b = 6 ;
+         //  [a, b] = [b, a] ;
+         //  console.log(a, b) ;
+      // 1.b ;
+         //   let a = 5 ;
+         //   let b = 6 ;
+         //   let temp ;
+         //   temp = a ;
+         //   a = b ;
+         //   b = temp ;
+         //   console.log(a, b) ;
+// cau 2 ;
+      // const s = 'Hello beauty there' ;
+      // const a = s.split(' ') ;
+      // console.log(a) ;
+// cau 3 ;
+         // const a = [4, 5, 7, -8] ;
+         // console.log(...a) ;
+// cau 4 ;
+      // let item =['Jeans', 'T-shirt', 'Socks'] ;
+      // do{
+         //  let a = prompt('Hi there, welcome to shop admin panel, what do you want(C,R,U,D)')
+         //  if ( a == " "){
+            //   break;
+         //  }
+      // else if (a == 'r'||a=='R'){
+               //   console.log('The current items are.')
+               //   for(let i in item){
+                     // console.log(`${Number(i)+1}. ${item[i]}` )
+               //   }
+            //  }
+            //  else if(a=='c'|| a=='C' ){
+               //   item.push = prompt ('Enter your name of the newitem ')
+               //   alert('Done')
+               //   console.log(item)
+            //  }
+            //  else if(a=='u'|| a=='U'){
+               //   let update = Number(prompt('Enter the position you want to update'));
+               //   let newName =  prompt("Enter the new name");
+               //   item[update-1] = prompt("Enter the new name");
+               //   alert('Done')
+               //   console.log(item);
+            //  }
+         // else if (a == 'd' || a == 'D'){
+                  //   let del = Number(prompt('Enter the position you want to delete'));
+                  //   item.splice(del-2,1);
+                  //   alert("Done");
+                  //   console.log(item);
+               //  }
+               //  else{
+                  //   alert('This command is not supported');
+               //  }
+                
+            // }while(1)
+// cau 5 ;
+      //  let n = prompt('Enter a sequence of Number seperated by commas (,)');
+      // let a = n.split(",");
+      // let sum = 0;
+      // for(i=0;i<a.length;i++)
+         //  {
+            //   a[i] = Number(a[i]);
+            //   sum +=a[i];   
+         //  }
+      // console.log(`The sum of them is ${sum}`);
+// cau 6 ;
+      // let n = prompt('Enter a sequence of numbers seperated by ","');
+      // let a = n.split(",");
+      // let min =Math.min.apply(Math,a);
+      // console.log('The smalest number is ' + min);
+// cau 7 ;
+      // const arr = [3, 4, 6, -9, 10, -88, 2];
+      // let n = Number(prompt('Enter a number :'));
+      // let index = arr.indexOf(n);
+      // if(index == -1){
+      // alert(`${n} is Not found in my array`)
+      // }
+      // else{
+      // alert(`${n} is FOUND in my array at index 3`)
+      // }
+// cau 8 ;
+   //   let size = [5,7,300,90,24,50,75];
+   //   console.log('Hello, my name is Phuong Nam and here is my sheep sizes :')
