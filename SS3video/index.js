08f94577a9218032160642da9579e3fa59f69648
hieulacos208 <hieulucas@gmail.com>
2021-03-26 08:05:47 +0700

    ok

---
+++ SS3video/index.js
@@ -0,0 +1,63 @@
+// alert('hello');
+// let yourName = prompt('xin chao, ten ban la gi vay ?');
+// alert('chao '+ yourName +' rat vui khi gap ban !');
+
+       // after 2 ;
+// let movies = ['batman', 'nguoi sat', 'nguoi nhen'];
+       // after 4 ;
+// movies.push('newMovie');
+// console.log(movies);
+        //after 5;
+// let i = prompt('Nhap vi tri ')
+// console.log(movies[i]);
+        //after 6 ;
+// let newMovie = prompt('Nhap ten phim :')
+// movies[0]= newMovie;
+// console.log(movies[0]); 
+
+// let i = prompt('Nhap vi tri ')
+// let newMovie = prompt('Nhap ten phim :')
+// movies[i]=newMovie;
+// console.log(movies)
+        //after 7 ;
+// let i = prompt('Nhap vi tri ')
+// movies.splice(i,1)
+// let i = prompt('Nhap vi tri ')
+// let n = prompt('Nhap so muon xoa ')
+// movies.splice(i,n)
+       // after 10 ;
+// console.log(movies)
+// for(let i =0; i<movies.length/2; i++)
+// {
+    // console.log(movies[i]);
+// }
+// for(let x in movies){
+        // console.log(`${Number(x)+1}. ${movies[x]}` )
+    // }
+      // after 11 ;
+// for(let i in movies){
+    // movies[i] = movies[i].toLowerCase();
+// }
+// console.log(movies)
+       //after 14 ;
+// let yourName = prompt("Register an username ")
+// if(yourName.length<=15){
+    // alert('Good username')
+// }
+// else{
+    // alert('Your user name is too long')
+// }
+
+
+// let choose = prompt ('How many legs does a spider have?\1. None\2. 4 legs\3. 8 legs\4. 12legs ')
+// do{
+// if(choose == 1 || choose == 2 || choose == 4){
+    // alert('Good luck next time')
+// }
+// else if(choose == 3){
+    // alert('Bravo, you are correct')
+// }
+// else{
+    // alert('Invalide choice, the answer must be 1, 2, 3 or 4')
+// }
+// }while(true);
