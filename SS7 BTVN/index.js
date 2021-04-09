e633ecdfc54eb2798f9500fa0f1a6dd448cfaabc
hieulacos208 <hieulucas@gmail.com>
2021-04-09 16:25:59 +0700

    ok

--- SS7 BTVN/index.js
+++ SS7 BTVN/index.js
@@ -1,146 +1,103 @@
 //ex1
 // console.log ('1-d, 2-a, 3-c, 4-b');
 //ex2
-// let timer= setInterval(() => alert('Hello'), 3000)
-// setTimeout(() => { clearInterval(timer); alert('Bye'); }, 6000);
-//ex3
-// console.log('No')
+// let timer= setInterval(() => console.log('Heehaw'), 3000)
+// setTimeout(() => { clearInterval(timer) }, 6000);
+// ex3
+// console.log('No') ;
+// console.log('No') ;
+
 // ex4
-// function ex4() {
-//     var x = document.getElementsByTagName("LI");
-//     document.getElementById("demo").innerHTML = x[1].innerHTML;
-//     console.log(x[1])
-//     for(let i=0; i<x.length; i++){
-//         console.log(x[i].innerHTML)
-//   }
+// function ex4(){
+//     var arr  = document.getElementsByTagName("li");
+//     console.log(arr[1]);
 // }
 // //ex5
-// function ex5() {
-//     let x = document.getElementsByTagName("div");
-//     document.getElementsByClassName("singer").innerHTML = x[1].innerHTML;
-//     console.log(x[1])
-//     for(let i=1; i<x.length; i++){
-//         console.log(x[i].innerHTML)
-//     }
+// function ex5(){
+//     var arr  = document.getElementsByClassName("Singer");
+//     console.log(arr[2]);
 // }
-// function ex71() {
-//     let btn1 = document.getElementById('button1')
-//     btn1.addEventListener('click', (e) => {
-//         console.log(e.target)
-//     });   
-//     let btn2 = document.getElementById('button2')
-//     btn2.addEventListener('click', (e) => {
-//         console.log(e.target)
-//     });  
-//     console.log('e.target is the button tag that was clicked')
+//Ex6:
+// function ex6(){
+//     var arr = document.getElementById('Remove');
+//     arr.remove();
 // }
-// function ex72() {
-//     let btn1 = document.getElementById('button1')
-//     btn1.addEventListener('keydown', (e) => {
-//         console.log(e.key)
-//     });   
-//     let btn2 = document.getElementById('button2')
-//     btn2.addEventListener('keydown', (e) => {
-//         console.log(e.key)
-//     });  
-//     console.log('e.key is the keypad that was pressed by user')
+// ex6();
+// Ex7
+// function ex71(){
+//     var btn1 = document.getElementById('button1');
+//     btn1.addEventListener('click',(e)=>{console.log(e.target);});
+//     var btn2 = document.getElementById('button2');
+//     btn2.addEventListener('click',(e)=>{console.log(e.target);});
 // }
+// function ex72(){
+//     var btn1 = getElementById('button1');
+//     btn1.addEventListener('keydown',(e)=>{console.log(e.key);})
+// }
+// Ex8 
 // function ex8() {
 //     alert('My name is Hieu')
 //     alert('Nam nay toi se giau')
 // }
-// function ex9(name,wish) {
-//     alert(`Your name is ${name}`)
-//     alert(`Your wish is ${wish}`)
-// }
-// ex9('Hieu','Nam nay toi giau')
-// function ex10(name,wish) {  
-//     alert(`Your name is ${name}`)
-//     if(!wish){
-//         alert('Your doesn\'t have any wish')
-//     }else{
-//         alert(`Your wish is ${wish}`)
+// Ex9 
+// function Ex9() {
+//     function list(n, w) {
+//     alert (`You are ${n} and one thing you wish to do this year is ${w}`);
 //     }
+//     let name = prompt ('What is your name?');
+//     let wish = prompt ('What is your wish?');
+//     list(name, wish);
+// }   
+// Ex9()
+// Ex10 
+// function ex10(name,wish) {  
+    // alert(`User's name is ${name}`)
+    // if(!wish){
+        // alert('User doesn\'t have any wish')
+    // }else{
+        // alert(`User's wish is ${wish}`)
+    // }
 // }
-// ex10('Hieu')
-function ex11(){
-    let nhap = document.getElementById('btn1')
-    console.log(nhap)
+// ex10('hieu','nam nay toi giau')
+// Ex11 
+// function ex11(){
+    // let a = document.getElementById('btn1')
+    // console.log(a)
     
-    let nhap1 = document.getElementById('input1')
-    console.log(nhap1)
+    // let b = document.getElementById('input1')
+    // console.log(b)
 
 
-    nhap.addEventListener('click',()=>{
-        console.log('Upper button clicked')
-    })
-}
+    // a.addEventListener('click',()=>{
+        // console.log('Upper button clicked')
+    // })
+// }
 // ex11()
-function ex11_5(){
-    let nhap = document.getElementById('title1')
-    console.log(nhap)
+// function ex11_5(){
+    // let b = document.getElementById('title1')
+    // console.log(b)
 
-    let x = document.getElementById('input1').value
-    let a = document.getElementById('btn1')
-    a.addEventListener('click',()=>{
-       return nhap.innerHTML = x.toUpperCase()
+    // let x = document.getElementById('input1').value
+    // let a = document.getElementById('btn1')
+    // a.addEventListener('click',()=>{
+    //    return b.innerHTML = x.toUpperCase()
+    //    console.log(x.value)
            
-    })   
-}
+    // })   
+// }
+// ex11_5()
 
-
-let items = ['Backpack','MiBand watch','Ring']
-let a = document.getElementById('item_list_ul')
-let b = document.getElementById('li2')
-let c = document.getElementById('li1')
-a.removeChild(c)
-a.removeChild(b)
-
-for (let i = 0; i < items.length; i++){
-    a.insertAdjacentHTML('beforeend',`<li><span>${items[i]}</span></li>`)
-
-} 
-
-let x = document.getElementsByTagName("LI");
-console.log(x[1])
-
-x[0].insertAdjacentHTML('beforeend',`<button id="r1" onclick="remove1()">Remove</button>`)
-x[1].insertAdjacentHTML('beforeend',`<button id="r2" onclick="remove2()">Remove</button>`)
-x[2].insertAdjacentHTML('beforeend',`<button id="r3" onclick="remove3()">Remove</button>`)
-
-
-
-function remove1(){
-    console.log('Clicked remove')
-    console.log(`Index: 0`)
-    items.splice(0, 1)
-    console.log(items)
-    x[0].remove()
-}
-function remove2(){
-    console.log('Clicked remove')
-    console.log(`Index: 1`)
-    items.splice(1, 1)
-    console.log(items)
-    x[1].remove()
-}
-function remove3(){
-    console.log('Clicked remove')
-    console.log(`Index: 2`)
-    items.splice(2, 1)
-    console.log(items)
-    x[2].remove()
-}
-
-function addNew(){
-    let a = document.getElementById('item_list_ul')
-    let e = document.getElementById('input2').value
-    
-    console.log('clicked')
-    console.log(e)
-    items.push(e)
-    console.log(items)
-    a.insertAdjacentHTML('beforeend',`<li><span>${e}</span></li>`)
-    console.log(a)
-
-}
+// Ex12 
+// let items = ['Backpack', 'Miband watch', 'Ring'];
+//     console.log(items);
+//     let items1 = document.getElementById("items1");
+//     console.log(items1);
+//     for (let i=0; i<items.length; i++) {
+//         items1.insertAdjacentHTML ('beforeend', 
+//         `<li>${items[i]}</li>`
+//         )
+//     }
+//     let test = document.getElementsByClassName("test");
+//     while(test.length > 0) {
+//         test[0].remove();
+//     }
